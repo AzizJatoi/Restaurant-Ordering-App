4f5f05d32e7f3a8f6b2ce44cffed38dcd980c94e
Aziz <91471349+AzizJatoi@users.noreply.github.com>
2024-02-12 18:04:00 +0500

    practice

--- index.js
+++ index.js
@@ -1,59 +1,29 @@
-import { menuArray } from "./data.js";
+import { menuArray } from './data.js'
 
-document.addEventListener("click", function(e) {
-    if (e.target.classList.contains("plus-icon")) {
-        const orderId = e.target.dataset.order;
-        toggleOrderSummary(orderId);
-    }
-});
 
-const container = document.getElementById("container");
 
-function getMenuArray() {
-    let restaurantMenu = '';
-    menuArray.forEach(item => {
-        restaurantMenu += `
-            <div class="container">
-                <div class="item-container">
-                    <div class="item-details">
-                        <p class="item-emoji">${item.emoji}</p>
-                        <div class="item-detail">
-                            <p class="item-name">${item.name}</p>
-                            <p class="item-ingredients">${item.ingredients}</p>
-                            <p class="item-price">$${item.price}</p>
-                        </div>
-                    </div>
-                    <p class="plus-icon" data-order="${item.id}">+</p>
+function getMenu() {
+    let menuItem = ''
+    menuArray.forEach((item) => 
+        menuItem += `
+            <div class="item-container">
+                <div class="item-details">
+                    <p class="item-img">${item.emoji}</p>
+                <div class="item-detail">
+                    <p class="item-name">${item.name}</p>
+                    <p class="item-ingredients">${item.ingredients}</p>
+                    <p class="item-price">$${item.price}</p>
                 </div>
+                </div>
+            <p class="plus-icon" data-order="${item.id}">+</p>
             </div>
-            <div class="order-summary hidden" id="order-summary-${item.id}">
-                <p class="bold-heading-center">Your Order</p>
-                <div class="order-container">
-                    <div class="item-details">
-                        <p class="item-name">${item.name}</p>
-                        <div class="item-detail">
-                            <p class="item-remove">remove</p>
-                        </div>
-                    </div>
-                    <p class="item-price">$${item.price}</p>
-                </div>
-                <div class="order-container">
-                    <div class="item-details">
-                        <p class="item-name">Total Price:</p>
-                    </div>
-                    <p class="item-price">$${item.price}</p>
-                </div>
-            </div>`;
-    });
-    return restaurantMenu;
-}
+        `
+    )
+    return menuItem
+} 
+
 
 function render() {
-    container.innerHTML += getMenuArray();
+    document.getElementById("container").innerHTML += getMenu()
 }
-render();
-
-function toggleOrderSummary(orderId) {
-    const orderSummary = document.getElementById(`order-summary-${orderId}`);
-    orderSummary.classList.toggle("hidden");
-}
+render()
